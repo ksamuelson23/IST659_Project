e0c9fd7537b292baddf1b9a0088b9ad9536f7beb
ksamuelson23 <ksamuelson23@gmail.com>
2025-03-20 09:35:34 -0400

    Add files via upload

---
+++ categories.sql
@@ -0,0 +1,8 @@
+create table categories (
+	category_id VARCHAR(1),
+	category_name VARCHAR(1)
+);
+insert into categories (category_id, category_name) values (1, 'Animal Product');
+insert into categories (category_id, category_name) values (2, 'Fruit');
+insert into categories (category_id, category_name) values (3, 'Meat');
+insert into categories (category_id, category_name) values (4, 'Vegetable');
